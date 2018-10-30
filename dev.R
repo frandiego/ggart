ecf761f0ad4a63306a6fdb85053a367cc0cc7a24
frandiego <franciscojosediegoacosta@gmail.com>
2018-10-30 18:04:52 +0100

    continue quijote

--- dev.R
+++ dev.R
@@ -6,6 +6,12 @@ first_sentence = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, 
 last_sentence = "Quijote, van ya tropezando, y han de caer del todo, sin duda alguna. Vale."
 chapter_word <- 'Capítulo'
 nlines <- 1000
+add_random_spaces <- function(x,y){
+  smp <- sample(seq_along(x),y)
+  x[smp] <- x[smp] %>% map(~paste0(.,' '))
+  return(paste0(x,collapse = ''))
+}
+
 # set LC_ALL
 Sys.setlocale("LC_ALL", "ES_ES.UTF-8")
 # read
@@ -20,10 +26,13 @@ txt_ <- paste0(txt,collapse = ' ')
 nchar_ <- ceiling(nchar(txt_)/nlines)
 
 
+
+
 l <- list()
 txt_ %>%  strsplit(' ') %>% unlist() -> txtw_
 txtw_ %>% map_int(nchar) -> txtwl_
 dtext <- data.table(word = txtw_, nchar = txtwl_)
+dtext[,nchar := nchar+1]
 dtext[,idx := .I]
 
 for (i in 1:nlines){
@@ -33,8 +42,19 @@ for (i in 1:nlines){
   dtext <- dtext[idx>idx_]
 }
 
+l_ <- unlist(l)
+txtwp_ <- txtw_ %>% map(~paste0(.,' '))
+textl_ <- map2(.x = c(1,head(l_,length(l_)-1)),.y = l_,.f = function(x,y){txtwp_[x:y]})
+texll_ <- map_int(textl_,~map_int(.,~nchar(.)) %>% length)
+maxchar_ <- max(texll_)
+texll_ %>% map_int(~maxchar_ - .)-> to_add
+map2(.x = textl_,.y = to_add,.f = function(x,y) add_random_spaces(x,y)) -> textr_
+textr_ %>% map(~paste0(.,'\n')) -> textr_
+textf <- paste0(textr_)
 
-
+ggdata <- data.table(text = textf)
+ggplot(ggdata,aes(y=1,x=1, label = text))+
+  geom_text()
 
 
 
